0a1dd7c1bd535d3883f78ebe81e81db05f736e09
Stephen Yingling <smy2748@rit.edu>
2013-10-25 15:13:58 -0400

    I don't like view tests.  Also, these default tests need to not be running.

--- target/scala-2.10/src_managed/main/views/html/main.template.scala
+++ target/scala-2.10/src_managed/main/views/html/main.template.scala
@@ -33,12 +33,43 @@ Seq[Any](format.raw/*1.32*/("""
 <html>
     <head>
         <title>"""),_display_(Seq[Any](/*7.17*/title)),format.raw/*7.22*/("""</title>
-        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*8.54*/routes/*8.60*/.Assets.at("stylesheets/main.css"))),format.raw/*8.94*/("""">
+        <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*8.54*/routes/*8.60*/.Assets.at("stylesheets/bootstrap.min.css"))),format.raw/*8.103*/("""">
         <link rel="shortcut icon" type="image/png" href=""""),_display_(Seq[Any](/*9.59*/routes/*9.65*/.Assets.at("images/favicon.png"))),format.raw/*9.97*/("""">
         <script src=""""),_display_(Seq[Any](/*10.23*/routes/*10.29*/.Assets.at("javascripts/jquery-1.9.0.min.js"))),format.raw/*10.74*/("""" type="text/javascript"></script>
-    </head>
+        <script src=""""),_display_(Seq[Any](/*11.23*/routes/*11.29*/.Assets.at("javascripts/bootstrap.min.js"))),format.raw/*11.71*/("""" type="text/javascript"></script>
+        <script src=""""),_display_(Seq[Any](/*12.23*/routes/*12.29*/.Assets.at("javascripts/bootstrap-tab.js"))),format.raw/*12.71*/("""" type="text/javascript"></script>
+        </head>
     <body>
-        """),_display_(Seq[Any](/*13.10*/content)),format.raw/*13.17*/("""
+        """),_display_(Seq[Any](/*15.10*/content)),format.raw/*15.17*/("""
+        <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Sphinx</a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Dashboard</a></li>
+              <li><a href="#about">Subscriptions</a></li>
+              <li><a href="#contact">Search</a></li>
+              <li><a href="#admin">Admin Tools</a></li>
+            </ul>
+            <form class="navbar-form navbar-right">
+              <div class="form-group">
+                <input type="text" placeholder="Email" class="form-control">
+              </div>
+              <div class="form-group">
+                <input type="password" placeholder="Password" class="form-control">
+              </div>
+              <button type="submit" class="btn btn-success">Sign in</button>
+            </form>
+          </div><!--/.navbar-collapse -->
+        </div>
+      </div>
     </body>
 </html>
 """))}
@@ -53,11 +84,11 @@ Seq[Any](format.raw/*1.32*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Thu Oct 17 17:29:21 EDT 2013
+                    DATE: Fri Oct 25 15:10:06 EDT 2013
                     SOURCE: /home/striker/work/Sphinx/app/views/main.scala.html
-                    HASH: 269b46d0510c6906d1a4d27ca715f76cc91ac485
-                    MATRIX: 778->1|902->31|990->84|1016->89|1113->151|1127->157|1182->191|1278->252|1292->258|1345->290|1406->315|1421->321|1488->366|1591->433|1620->440
-                    LINES: 26->1|29->1|35->7|35->7|36->8|36->8|36->8|37->9|37->9|37->9|38->10|38->10|38->10|41->13|41->13
+                    HASH: 5c4ebb6f5cc72e4e122a5f94d9cabe5215f8bf40
+                    MATRIX: 778->1|902->31|990->84|1016->89|1113->151|1127->157|1192->200|1288->261|1302->267|1355->299|1416->324|1431->330|1498->375|1591->432|1606->438|1670->480|1763->537|1778->543|1842->585|1949->656|1978->663
+                    LINES: 26->1|29->1|35->7|35->7|36->8|36->8|36->8|37->9|37->9|37->9|38->10|38->10|38->10|39->11|39->11|39->11|40->12|40->12|40->12|43->15|43->15
                     -- GENERATED --
                 */
             
