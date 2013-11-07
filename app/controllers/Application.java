fae95199e98fa198eeda3a425107b6ca0b8d3459
adl2158 <adl2158@acd413023125.wireless.rit.edu>
2013-11-07 00:57:27 -0500

    Adding comments to the control center.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,24 +3,9 @@ package controllers;
 import helpers.MongoControlCenter;
 
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import play.*;
 import play.mvc.*;
 
 import views.html.*;
-
-import com.mongodb.MongoClient;
-import com.mongodb.MongoException;
-import com.mongodb.WriteConcern;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
-import com.mongodb.DBCursor;
-import com.mongodb.ServerAddress;
 
 public class Application extends Controller {
 
@@ -28,7 +13,7 @@ public class Application extends Controller {
 		MongoControlCenter control = new MongoControlCenter("venture.se.rit.edu", 27017);
 		control.setDatabase("dev");
 
-		Object[] array = control.getInitiativesByUser("entities", "jay-z");
+		Object[] array = control.getEntitiesByUser("jay-z");
 		
 		control.closeConnection();
 
