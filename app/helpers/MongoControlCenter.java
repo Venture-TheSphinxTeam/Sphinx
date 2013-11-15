9e1d54341c89ad01150a85aae81948364f11de2a
adl2158 <adl2158@129.21.64.146>
2013-11-15 14:32:29 -0500

    Query for a users' teams' events.

--- app/helpers/MongoControlCenter.java
+++ app/helpers/MongoControlCenter.java
@@ -28,7 +28,8 @@ public class MongoControlCenter {
 	 * 
 	 * Sets the database of the Mongo DB controller.
 	 * 
-	 * @param dbName  The name of the Database to access
+	 * @param dbName
+	 *            The name of the Database to access
 	 */
 	public void setDatabase(String dbName) {
 		db = mongoClient.getDB(dbName);
@@ -42,121 +43,234 @@ public class MongoControlCenter {
 		mongoClient.close();
 	}
 
-
-
-        public Object[] getEventsForUser(String user) {
-
-          DBObject query = new QueryBuilder()
-				.or(new BasicDBObject("assignee", user))
-				.or(new BasicDBObject("watchers", user))
-				.or(new BasicDBObject("reporter", user))
-				.or(new BasicDBObject("businessOwner", user))
-				.get();
-
-
-
-          //ArrayList<Integer> ids = new ArrayList<Integer>();
-          
-          BasicDBList ids = new BasicDBList();
-
-          BasicDBObject eventQuery = new BasicDBObject("entity.entityId", new BasicDBObject("$in", ids));
-
-          DBCollection init = db.getCollection("initiatives");
-          DBCollection risks = db.getCollection("risks");
-          DBCollection mile = db.getCollection("milestones");
-          DBCollection event = db.getCollection("events");          
-
-          ArrayList<DBObject> result = new ArrayList<DBObject>();
-
-          DBCursor c = init.find(query); 
-	  DBObject t;
-          try {
-                while (c.hasNext()){
-		  t = c.next();
-                  ids.add(((BasicDBObject)t).getInt("entityId"));
-                }
-              }
-          finally{
-             c.close();
-          }
-
-          c = event.find(eventQuery.append("entity.entityType", "INITIATIVE"));
-
-         try{
-           while(c.hasNext()){
-             result.add(c.next());
-           }
-         }
-         finally{
-           c.close();
-         }
-
-        ids = new BasicDBList();
-
-	c = risks.find(query);
-	try{
-          while(c.hasNext()){
-	    t = c.next();
-	    //System.out.println(((BasicDBObject)t).getInt("entityId"));
-	    ids.add(((BasicDBObject)t).getInt("entityId"));
-	  }
-	}
-	finally{
-	  c.close();
-	}
-        eventQuery = new BasicDBObject("entity.entityId", new BasicDBObject("$in", ids));
-	c = event.find(eventQuery.append("entity.entityType","RISK"));
-        
-	try{
-	  while(c.hasNext()){
-	  result.add(c.next());}
-	}
-	finally{
-	  c.close();
-	}
-
-
-	ids = new BasicDBList();
-	c= mile.find(query);
-        try{
-          while(c.hasNext()){ids.add(((BasicDBObject)c.next()).getInt("entityId"));}
-	}
-	finally{
-	  c.close();
-	}
-        eventQuery = new BasicDBObject("entity.entityId", new BasicDBObject("$in", ids));
-	c = event.find(eventQuery.append("entity.entityType","MILESTONE"));
-        
-	try{
-	  while(c.hasNext()){result.add(c.next());}
-	}
-	finally{
-	  c.close();
-	}
-
-        return result.toArray();
-
-        }
-
-
-
-
-	/**
-	 * 
-	 * Gets all the entities the supplied user is a part of. 
-	 * 
-	 * 
-	 * @param user	The user supplied.
-	 * @return	An array of entity documents the user belongs to.
-	 */
-	public Object[] getEntitiesByUser(String user) {
+	public Object[] getEventsForUser(String user) {
 
 		DBObject query = new QueryBuilder()
 				.or(new BasicDBObject("assignee", user))
 				.or(new BasicDBObject("watchers", user))
 				.or(new BasicDBObject("reporter", user))
-				.or(new BasicDBObject("businessOwner", user))
-				.get();
+				.or(new BasicDBObject("businessOwner", user)).get();
+
+		// ArrayList<Integer> ids = new ArrayList<Integer>();
+
+		BasicDBList ids = new BasicDBList();
+
+		BasicDBObject eventQuery = new BasicDBObject("entity.entityId",
+				new BasicDBObject("$in", ids));
+
+		DBCollection init = db.getCollection("initiatives");
+		DBCollection risks = db.getCollection("risks");
+		DBCollection mile = db.getCollection("milestones");
+		DBCollection event = db.getCollection("events");
+
+		ArrayList<DBObject> result = new ArrayList<DBObject>();
+
+		DBCursor c = init.find(query);
+		DBObject t;
+		try {
+			while (c.hasNext()) {
+				t = c.next();
+				ids.add(((BasicDBObject) t).getInt("entityId"));
+			}
+		} finally {
+			c.close();
+		}
+
+		c = event.find(eventQuery.append("entity.entityType", "INITIATIVE"));
+
+		try {
+			while (c.hasNext()) {
+				result.add(c.next());
+			}
+		} finally {
+			c.close();
+		}
+
+		ids = new BasicDBList();
+
+		c = risks.find(query);
+		try {
+			while (c.hasNext()) {
+				t = c.next();
+				// System.out.println(((BasicDBObject)t).getInt("entityId"));
+				ids.add(((BasicDBObject) t).getInt("entityId"));
+			}
+		} finally {
+			c.close();
+		}
+		eventQuery = new BasicDBObject("entity.entityId", new BasicDBObject(
+				"$in", ids));
+		c = event.find(eventQuery.append("entity.entityType", "RISK"));
+
+		try {
+			while (c.hasNext()) {
+				result.add(c.next());
+			}
+		} finally {
+			c.close();
+		}
+
+		ids = new BasicDBList();
+		c = mile.find(query);
+		try {
+			while (c.hasNext()) {
+				ids.add(((BasicDBObject) c.next()).getInt("entityId"));
+			}
+		} finally {
+			c.close();
+		}
+		eventQuery = new BasicDBObject("entity.entityId", new BasicDBObject(
+				"$in", ids));
+		c = event.find(eventQuery.append("entity.entityType", "MILESTONE"));
+
+		try {
+			while (c.hasNext()) {
+				result.add(c.next());
+			}
+		} finally {
+			c.close();
+		}
+
+		return result.toArray();
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public Object[] getTeamEventsForUser(String user) {
+
+		DBObject query = new BasicDBObject("username", user);
+
+		DBCollection users = db.getCollection("users");
+
+		ArrayList<String> groups = new ArrayList<String>();
+
+		DBCursor cursor = users.find(query);
+
+		try {
+			while (cursor.hasNext()) {
+				groups = (ArrayList<String>) cursor.next().get("groups");
+			}
+		}
+
+		finally {
+			cursor.close();
+		}
+
+		ArrayList<DBObject> teamEntities = new ArrayList<DBObject>();
+		BasicDBList ids = new BasicDBList();
+
+		BasicDBObject eventQuery = new BasicDBObject("entity.entityId",
+				new BasicDBObject("$in", ids));
+
+		for (String group : groups) {
+
+			DBObject teamQuery = new QueryBuilder()
+					.or(new BasicDBObject("businessGroups", group))
+					.or(new BasicDBObject("providerGroups", group)).get();
+
+			DBCollection event = db.getCollection("events");
+
+			DBCollection init = db.getCollection("initiatives");
+
+			DBCursor teamCursor = init.find(teamQuery);
+			DBObject t;
+			try {
+				while (teamCursor.hasNext()) {
+					t = teamCursor.next();
+					ids.add(((BasicDBObject) t).getInt("entityId"));
+				}
+			} finally {
+				teamCursor.close();
+			}
+
+			teamCursor = event.find(eventQuery.append("entity.entityType",
+					"INITIATIVE"));
+
+			try {
+				while (teamCursor.hasNext()) {
+					teamEntities.add(teamCursor.next());
+				}
+			} finally {
+				teamCursor.close();
+			}
+
+			ids.clear();
+
+			DBCollection risks = db.getCollection("risks");
+
+			teamCursor = risks.find(teamQuery);
+
+			try {
+				while (teamCursor.hasNext()) {
+					t = teamCursor.next();
+					ids.add(((BasicDBObject) t).getInt("entityId"));
+				}
+			} finally {
+				teamCursor.close();
+			}
+
+			eventQuery = new BasicDBObject("entity.entityId",
+					new BasicDBObject("$in", ids));
+			teamCursor = event.find(eventQuery.append("entity.entityType",
+					"RISK"));
+
+			try {
+				while (teamCursor.hasNext()) {
+					teamEntities.add(teamCursor.next());
+				}
+			} finally {
+				teamCursor.close();
+			}
+
+			ids.clear();
+
+			DBCollection mile = db.getCollection("milestones");
+
+			teamCursor = mile.find(teamQuery);
+
+			try {
+				while (teamCursor.hasNext()) {
+					t = teamCursor.next();
+					ids.add(((BasicDBObject) t).getInt("entityId"));
+				}
+			} finally {
+				teamCursor.close();
+			}
+
+			eventQuery = new BasicDBObject("entity.entityId",
+					new BasicDBObject("$in", ids));
+			teamCursor = event.find(eventQuery.append("entity.entityType",
+					"MILESTONE"));
+
+			try {
+				while (teamCursor.hasNext()) {
+					teamEntities.add(teamCursor.next());
+				}
+			} finally {
+				teamCursor.close();
+			}
+
+		}
+
+		return teamEntities.toArray();
+	}
+
+	/**
+	 * 
+	 * Gets all the entities the supplied user is a part of.
+	 * 
+	 * 
+	 * @param user
+	 *            The user supplied.
+	 * @return An array of entity documents the user belongs to.
+	 */
+	public Object[] getInitiativesByUser(String user) {
+
+		DBObject query = new QueryBuilder()
+				.or(new BasicDBObject("assignee", user))
+				.or(new BasicDBObject("watchers", user))
+				.or(new BasicDBObject("reporter", user))
+				.or(new BasicDBObject("businessOwner", user)).get();
 		DBCollection coll = db.getCollection("initiatives");
 
 		DBCursor cursor = coll.find(query);
@@ -178,8 +292,9 @@ public class MongoControlCenter {
 	 * 
 	 * Gets all of the documents in a specific collection
 	 * 
-	 * @param collection	The collection being queried.
-	 * @return	An array of all documents in that collection.
+	 * @param collection
+	 *            The collection being queried.
+	 * @return An array of all documents in that collection.
 	 */
 	public Object[] getAllDocuments(String collection) {
 		DBCollection coll = db.getCollection(collection);
