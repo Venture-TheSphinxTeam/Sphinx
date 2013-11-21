a536acf9f6c76ab2df46848330751dee273124dd
Stephen Yingling <smy2748@rit.edu>
2013-11-20 21:49:51 -0500

    Added constructors and some basic scraping functionality

--- app/controllers/Ingester.java
+++ app/controllers/Ingester.java
@@ -7,9 +7,57 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
 public class Ingester {
+        protected Client client;
+	protected WebTarget webTarget;
+	protected Invocation.Builder invBuild;
+
+
+        /**
+        ***Creates an ingester with a default rest client
+	**/
+        public Ingester(){
+	  client = ClientBuilder.newClient();
+	}
+
+        /**
+	 **Creates an ingester pointing at the given url
+	 **/
+	public Ingester(String target){
+          client = ClientBuilder.newClient();
+	  webTarget = client.target(target);
+	  invBuild = webTarget.request();
+	}
+
+        /**
+	 **Gets the response from the set target and returns the message as a string
+	 **/
+	public String getResponse(){
+          Response r;
+	  
+	  if(invBuild != null){
+            r = invBuild.get();
+	    if(r.status == 200){
+	      return r.readEntity(String.class);
+	    }
+	  }
+
+	  return null;
+	}
+
+
+
+        public String getMessageFromTarget(String target){
+          WebTarget t = client.target(target);
+
+	  Invocation.Builder ib = t.request();
+
+	  Response r = ib.get();
+
+	  return r.readEntity(String.class);
+	}
+
+
 	public void getStuff(){
-		Client client = ClientBuilder.newClient();
-		
 		WebTarget target = client.target("http://localhost:9998");
 		
 		Invocation.Builder ib = target.request();
