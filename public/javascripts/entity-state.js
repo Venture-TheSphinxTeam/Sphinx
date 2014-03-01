9d3f8157c1a7212caf341102321c2e4885418c76
Samantha Shandrow <samantha.shandrow@gmail.com>
2014-03-01 11:58:28 -0500

    In progress

--- public/javascripts/entity-state.js
+++ public/javascripts/entity-state.js
@@ -42,7 +42,7 @@ function updateEntityStateStatus(entityType,entityId,username,buttonType){
  * Gets and updates status of all buttons of entityType-entityId on the page
  */
 function setStatusOfAllButtons(entityType,entityId,username){
-	setStatusOfButtons(entityType,entityId,username,'subscription');
+	setStatusOfButtons(entityType,entityId,username,"subscription");
 	//setStatusOfButtons(entityType,entityId,username,buttonType);
 	//setStatusOfButtons(entityType,entityId,username,buttonType);
 }
@@ -58,17 +58,8 @@ function setStatusOfButtons(entityType,entityId,username,buttonType){
 				 'buttonType': buttonType };
 
 	var url = '/getButtonStatus';
-	// set url to post data to ie. to subscribe, vote or watch
-	/*var url;
-	if( buttonType.toLowerCase() == 'subscription'){
-		url = "/getSubscriptionStatus";
-	}
-	else if(  buttonType.toLowerCase() == 'watch'){
-		//url = "/getWatchStatus"
-	}
-	else{	// vote
-		//url = "/getVoteStatus"
-	}*/
+
+	alert(buttonType);
 
 	//Need to get a hole of the button element itself.//
 	$.ajax({
@@ -79,9 +70,7 @@ function setStatusOfButtons(entityType,entityId,username,buttonType){
 		contentType: 'application/json; charset=utf-8',
 		success: function (data){
 			// update all buttons of buttenType on the page
-			updateButtonsOnPage(entityType,entityId,data['status'], "subscription");
-			//updateButtonsOnPage(entityType,entityId,data['status'], "vote");
-			//updateButtonsOnPage(entityType,entityId,data['status'], "watch");
+			updateButtonsOnPage(entityType,entityId,data['status'], buttonType);
 		}
 	})
 }
