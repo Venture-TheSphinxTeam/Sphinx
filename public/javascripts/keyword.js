10810f2961227d70f057a75e9d5871b2e69d9be8
adl2158 <adl2158@acd413023125.wireless.rit.edu>
2014-03-04 16:07:29 -0500

    refactoring radio button

--- public/javascripts/keyword.js
+++ public/javascripts/keyword.js
@@ -4,7 +4,7 @@
 
 function keywordSearch(){
 var patt = document.getElementById("entitySearch").value;
-var radio_field = $("input:radio[name='keyword']:checked").val();
+var radio_field = $('.btn.btn-primary.active').attr('id');
 window.location.href = "./search?arg=" + patt + "&field=" + radio_field;
 }
 
