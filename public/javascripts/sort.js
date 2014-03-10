34a672c5d0f36677b1cdabcae3a942e711a9ff0b
adl2158 <adl2158@129.21.65.22>
2014-03-10 19:31:06 -0400

    Dummy buttons

--- public/javascripts/sort.js
+++ public/javascripts/sort.js
@@ -3,6 +3,8 @@
  */
 
 $('#entities').html($("#entities li").sort(asc_sort));
-function asc_sort(a, b){
-    return ($(b).find('h3:first-child').text()) < ($(a).find('h3:first-child').text()) ? 1 : -1;    
+
+function asc_sort(a, b) {
+	return ($(b).find('h3:first-child').text()) < ($(a).find('h3:first-child')
+			.text()) ? 1 : -1;
 }
