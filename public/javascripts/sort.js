79628b11d51dac97cbd13e828a98567adf797f32
adl2158 <adl2158@129.21.65.22>
2014-03-18 13:48:58 -0400

    Sorting asc working for proposed and recent date now.

--- public/javascripts/sort.js
+++ public/javascripts/sort.js
@@ -2,10 +2,9 @@
  * 
  */
 
-
 function sortAlpha() {
-	$('#entities').html($("#entities li").sort(asc_sort));
-	
+	$('#entities').html($("#entities li").sort(alpha_sort));
+
 	$(document).ready(function() {
 		$('#page_container').pajinate({
 			items_per_page : 15,
@@ -14,7 +13,39 @@ function sortAlpha() {
 	});
 }
 
-function asc_sort(a, b) {
+function sortProposed(){
+	$('#entities').html($("#entities li").sort(proposed_sort));
+
+	$(document).ready(function() {
+		$('#page_container').pajinate({
+			items_per_page : 15,
+			abort_on_small_lists : true
+		});
+	});
+}
+
+function sortUpdated(){
+	$('#entities').html($("#entities li").sort(recent_sort));
+
+	$(document).ready(function() {
+		$('#page_container').pajinate({
+			items_per_page : 15,
+			abort_on_small_lists : true
+		});
+	});
+}
+
+function alpha_sort(a, b) {
 	return ($(b).find("#summary").text()) < ($(a).find("#summary").text()) ? 1
+			: -1;
+}
+
+function proposed_sort(a, b) {
+	return ($(b).find("#proposed").text()) > ($(a).find("#proposed").text()) ? 1
+			: -1;
+}
+
+function recent_sort(a, b){
+	return ($(b).find("#updated").text()) > ($(a).find("#updated").text()) ? 1
 			: -1;
 }
