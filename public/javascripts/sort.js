175d3ea22c6747951d92b2f9b780ca6cac91b46d
adl2158 <adl2158@129.21.65.22>
2014-03-18 14:04:15 -0400

    Can now sort asc and desc for each sort option.

--- public/javascripts/sort.js
+++ public/javascripts/sort.js
@@ -2,8 +2,15 @@
  * 
  */
 
-function sortAlpha() {
-	$('#entities').html($("#entities li").sort(alpha_sort));
+function sortAlpha(desc) {
+	if (desc) {
+		$('#entities').html($("#entities li").sort(desc_alpha_sort));
+	}
+
+	else {
+		$('#entities').html($("#entities li").sort(alpha_sort));
+
+	}
 
 	$(document).ready(function() {
 		$('#page_container').pajinate({
@@ -13,9 +20,13 @@ function sortAlpha() {
 	});
 }
 
-function sortProposed(){
-	$('#entities').html($("#entities li").sort(proposed_sort));
+function sortProposed(desc) {
 
+	if (desc) {
+		$('#entities').html($("#entities li").sort(desc_proposed_sort));
+	} else {
+		$('#entities').html($("#entities li").sort(proposed_sort));
+	}
 	$(document).ready(function() {
 		$('#page_container').pajinate({
 			items_per_page : 15,
@@ -24,9 +35,13 @@ function sortProposed(){
 	});
 }
 
-function sortUpdated(){
-	$('#entities').html($("#entities li").sort(recent_sort));
+function sortUpdated(desc) {
 
+	if (desc) {
+		$('#entities').html($("#entities li").sort(desc_recent_sort));
+	} else {
+		$('#entities').html($("#entities li").sort(recent_sort));
+	}
 	$(document).ready(function() {
 		$('#page_container').pajinate({
 			items_per_page : 15,
@@ -40,12 +55,27 @@ function alpha_sort(a, b) {
 			: -1;
 }
 
+function desc_alpha_sort(a, b) {
+	return ($(b).find("#summary").text()) > ($(a).find("#summary").text()) ? 1
+			: -1;
+}
+
 function proposed_sort(a, b) {
 	return ($(b).find("#proposed").text()) > ($(a).find("#proposed").text()) ? 1
 			: -1;
 }
 
-function recent_sort(a, b){
+function desc_proposed_sort(a, b) {
+	return ($(b).find("#proposed").text()) < ($(a).find("#proposed").text()) ? 1
+			: -1;
+}
+
+function recent_sort(a, b) {
 	return ($(b).find("#updated").text()) > ($(a).find("#updated").text()) ? 1
+			: -1;
+}
+
+function desc_recent_sort(a, b) {
+	return ($(b).find("#updated").text()) < ($(a).find("#updated").text()) ? 1
 			: -1;
 }
