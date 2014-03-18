b022a72a7096a23e0be9191d3efa95e30397c2fb
Stephen Yingling <smy2748@rit.edu>
2014-03-18 19:21:10 -0400

    Installation script

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#git the project
+git clone https://github.com/Venture-TheSphinxTeam/Sphinx.git
+
+cd Sphinx
+
+MACHINE_TYPE = 'uname -m'
+
+# Install the pre-reqs with a package manager.
+if [[ $* == *--no-prereqs* ]]; then
+	echo "Prereq installation skipped."
+elif command -v apt-get > /dev/null; then
+	echo "Installing prereqs with apt-get..."
+  	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+	echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+	sudo apt-get update
+	sudo apt-get install mongodb-10gen
+else
+	echo "Installing the hard way..."
+	if [ ${MACHINE_TYPE} == 'x86_64']; then
+		echo "64 bit architecture detected"
+		curl -O http://downloads.mongodb.org/linux/mongodb-linux-x86_64-2.4.9.tgz
+		tar -zxvf mongodb-linux-x86_64-2.4.9.tgz
+		mkdir -p mongodb
+		cp -R -n mongodb-linux-x86_64-2.4.9/ mongodb
+	else
+		echo "32 bit architecture detected"
+		curl -O http://downloads.mongodb.org/linux/mongodb-linux-i686-2.4.9.tgz
+		tar -zxvf mongodb-linux-i686-2.4.9.tgz
+		mkdir -p mongodb
+		cp -R -n mongodb-linux-i686-2.4.9/ mongodb
+	fi
+	mkdir -p /data/db
+	chown mongodb /data/db
+fi
+
+
+sudo service mongodb start
+
+curl -O http://downloads.typesafe.com/play/2.2.2/play-2.2.2.zip
+mkdir -p /opt/play
+unzip play-2.2.2.zip -d /opt/play
+
+
