66748b89444889943d1df95833b8e7e63d83ae6d
Stephen Yingling <smy2748@rit.edu>
2014-03-18 22:14:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,10 +51,9 @@ echo "Installing play in /opt/play"
 curl -O http://downloads.typesafe.com/play/2.2.2/play-2.2.2.zip
 sudo mkdir -p /opt/play
 sudo unzip play-2.2.2.zip -d /opt/play
-sudo cp -r /opt/play/play-2.2.2.zip/* /opt/play/
+sudo cp -r /opt/play/play-2.2.2/* /opt/play/
 rm play-2.2.2.zip
 echo "export PATH=$PATH:/opt/play" | sudo tee -a /etc/bash.bashrc
-export PATH="$PATH:/opt/play"
 
 #git the project
 git clone https://github.com/Venture-TheSphinxTeam/Sphinx.git
