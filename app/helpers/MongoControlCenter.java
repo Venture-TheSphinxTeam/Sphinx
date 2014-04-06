267f36984b1dfa144a3db31aa793110375f4ede4
adl2158 <adl2158@192.168.1.143>
2014-04-06 00:27:35 -0400

    Removing print statements. Sorry for commit spam.

--- app/helpers/MongoControlCenter.java
+++ app/helpers/MongoControlCenter.java
@@ -472,7 +472,6 @@ public class MongoControlCenter {
 	}
 
 	public ArrayList<Entity> getEntitiesByQuery(String query) {
-		System.out.println(query);
 		ArrayList<Entity> result = new ArrayList<Entity>();
 
 		Iterator<? extends Entity> initIter = Initiative.findBy(query)
@@ -492,7 +491,6 @@ public class MongoControlCenter {
 		while (riskIter.hasNext()) {
 			result.add(riskIter.next());
 		}
-		System.out.println(result);
 		return result;
 
 	}
