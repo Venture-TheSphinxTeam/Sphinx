49269d9c9d0c8e08172850f48b23dfc750f2245f
adl2158 <adl2158@192.168.1.143>
2014-04-06 00:10:57 -0400

    Adding allowed access users query to children.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -261,7 +261,15 @@ public class Application extends Controller {
 			if (((entity_Initiative.getAllowedAccessUsers().contains(USERNAME) || ((entity_Initiative
 					.getAllowedAccessUsers().isEmpty()))))) {
 
-				return ok(initiative.render(entity_Initiative, USERNAME, control.getEntitiesByQuery("\"workBreakdownParent.entityId\":" + "\"" + entity_Initiative.getEntityId() + "\"")));
+				return ok(initiative
+						.render(entity_Initiative,
+								USERNAME,
+								control.getEntitiesByQuery("\"workBreakdownParent.entityId\":"
+										+ "\""
+										+ entity_Initiative.getEntityId()
+										+ "\","
+										+ control
+												.createAllowedAccessUsersQuery(USERNAME))));
 			} else {
 				return ok(accessError.render());
 			}
