6de5486bbc0254f2d93dd377f2cd4950ee41eb55
cmatlock <chanel.matlock@gmail.com>
2014-04-12 16:30:15 -0400

    Comment modal style matches everyone else. Comment header is no longer shown. Bit of reformating of the commenting area so it's prettier.

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -1,3 +1,76 @@
+
+function setCommentBox_New(entityType, entityId, createdBy){
+	var allFields = $([]).add(commentHeader).add(comment);
+
+	$("#modal-label").html("New Comment");
+
+	$("#modal-body-text").html("Enter your new comment and hit Submit. ");
+
+	$("#modal-form").html("<input type='text' name='comment' id='comment'>")
+
+	$("#modal-button1").html("Cancel");
+	$("#modal-button1").removeClass();
+	$("#modal-button1").addClass("btn btn-primary");
+
+	$("#modal-button2").html("Sumbit");
+	$("#modal-button2").removeClass();
+	$("#modal-button2").addClass("btn btn-default");
+
+	$("#modal-button2").click(
+		function(){
+			var comment = $("#comment");
+			//will need to prevent submitting empty comments
+			createComment(entityType, entityId, createdBy, "", comment.val())
+		}
+	);
+
+}
+
+function setCommentBox_Edit(){
+	var allFields = $([]).add(commentHeader).add(comment);
+
+	$("#modal-label").html("Edit Comment");
+
+	$("#modal-body-text").html("Edit your comment and hit Submit. ");
+
+	$("#modal-button1").html("Cancel");
+	$("#modal-button1").removeClass();
+	$("#modal-button1").addClass("btn btn-primary");
+
+	$("#modal-button2").html("Sumbit");
+	$("#modal-button2").removeClass();
+	$("#modal-button2").addClass("btn btn-default");
+
+	$("#modal-button2").click(
+		function(){
+			editComment(entityType, entityId, createdBy, "", comment)
+		}
+	);
+
+}
+
+function setCommentBox_Delete(){
+	var allFields = $([]).add(commentHeader).add(comment);
+
+	$("#modal-label").html("Delete Comment");
+
+	$("#modal-body-text").html("Are you sure you want to delete your comment? ");
+
+	$("#modal-button1").html("Cancel");
+	$("#modal-button1").removeClass();
+	$("#modal-button1").addClass("btn btn-primary");
+
+	$("#modal-button2").html("Delete");
+	$("#modal-button2").removeClass();
+	$("#modal-button2").addClass("btn btn-default");
+
+	$("#modal-button2").click(
+		function(){
+			deleteComment(entityType, entityId, createdBy, "", comment)
+		}
+	);
+}
+
 function createComment(entityType, entityId,createdBy,commentHeader,comment){
 
 	var json = { 'entityType': entityType, 
@@ -15,28 +88,21 @@ function createComment(entityType, entityId,createdBy,commentHeader,comment){
 		datatype: "json",
 		contentType: 'application/json; charset=utf-8',
 		success: function(){
-			//$("#entity-comments").load("/ #comment-container div");
 			window.location.reload();
 		}
 	})
 
-	//$.ajax({
-	//			type: "GET",
-	//			url: $(this).attr('href'), //current url
-				//data: JSON.stringify(json),
-				//datatype: "json",
-	//			contentType: 'application/json; charset=utf-8'
-	//		})
 }
 
-function editComment(commentHeader,comment){
+function editComment(entityType, entityId, createdBy, commentHeader,comment){
+	//NOT COMPLETED
 	var json = { 'entityType': entityType, 
 	'entityId': 	 entityId,
 	'createdBy':   createdBy,
 	'commentHeader': commentHeader,
-	'comment': comment,  };
+	'comment': comment  };
 
-	var url = "";
+	var url = "/newComment";
 
 	$.ajax({
 		type: "POST",
@@ -44,6 +110,30 @@ function editComment(commentHeader,comment){
 		data: JSON.stringify(json),
 		datatype: "json",
 		contentType: 'application/json; charset=utf-8',
-		
+		success: function(){
+			window.location.reload();
+		}
+	})
+}
+
+function deleteComment(entityType, entityId, createdBy, commentHeader,comment){
+	//NOT COMPLETED
+	var json = { 'entityType': entityType, 
+	'entityId': 	 entityId,
+	'createdBy':   createdBy,
+	'commentHeader': commentHeader,
+	'comment': comment  };
+
+	var url = "/newComment";
+
+	$.ajax({
+		type: "POST",
+		url: url,
+		data: JSON.stringify(json),
+		datatype: "json",
+		contentType: 'application/json; charset=utf-8',
+		success: function(){
+			window.location.reload();
+		}
 	})
 }
